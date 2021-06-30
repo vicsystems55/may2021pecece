054bb1d42cb75b186cde3aaff92ebc81b2dc9513
vicSystems <victorasuquob@gmail.com>
2021-06-30 09:07:32 +0100

    ui clean up

--- resources/views/front_page/inc/menu_bar.blade.php
+++ resources/views/front_page/inc/menu_bar.blade.php
@@ -15,14 +15,14 @@
                                    <a href="{{route('blog')}}">Blog</a>
                                 
                                </li>
-                               <li>
+                               {{-- <li>
                                    <a href="{{route('about')}}">About</a>
                                
                                </li>
                                <li>
                                    <a href="{{route('contact')}}">Contact</a>
                                 
-                               </li>
+                               </li> --}}
                           
                            </ul>
                        </div>
